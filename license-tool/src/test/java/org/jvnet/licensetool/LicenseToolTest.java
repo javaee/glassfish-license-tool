b2ea358a65a9b7c8f2324d231ec3c5d0c465dae3
Rama Pulavarthi <ramapulavarthi@java.net>
2008-01-03 23:10:19 +0000

    Adding a testcase
    svn path=/trunk/; revision=16

---
+++ license-tool/src/test/java/org/jvnet/licensetool/LicenseToolTest.java
@@ -0,0 +1,63 @@
+package org.jvnet.licensetool;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Unit test for LicenseTool * 
+ */
+public class LicenseToolTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public LicenseToolTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( LicenseToolTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {   List<String> args = new ArrayList<String>();
+        args.add("-validate");
+        args.add("true");
+        //args.add("-verbose");
+        //args.add("true");
+        args.add("-roots");
+        args.add(new File(getRoot(),"target/test-classes/testsrc").getPath());
+        args.add("-skipdirs");
+        args.add(".svn");
+        args.add("-copyright");
+        args.add(new File(getRoot(),"target/test-classes/copyright/copyright.txt").getPath());
+        LicenseTool.main(args.toArray(new String[0]));
+    }
+
+    private File getRoot() {
+        String classnameAsResource  = this.getClass().getName().replace('.', '/') + ".class";
+        URL res = getClass().getClassLoader().getResource(classnameAsResource);
+        File f = new File(res.getFile());
+        while(!new File(f,"pom.xml").exists())
+            f = f.getParentFile();
+        return f;
+    }
+}
