ed8a7c18e6b397d195304754d72db0f27e84bc1f
Rama Pulavarthi <ramapulavarthi@java.net>
2008-01-08 18:56:21 +0000

    Fix for comments starting and ending in the same line.
    svn path=/trunk/; revision=36

--- license-tool/src/main/java/org/jvnet/licensetool/CommentBlock.java
+++ license-tool/src/main/java/org/jvnet/licensetool/CommentBlock.java
@@ -9,29 +9,31 @@ import java.util.Iterator;
 /**
  * Marker for CommentBlock
  */
-public class CommentBlock extends Block{
-    protected Pair<String,String> commentStart = null;
-    protected List<Pair<String, String>> commentLines = new ArrayList<Pair<String,String>>();
-    protected Pair<String,String> commentEnd = null;
+public class CommentBlock extends Block {
+    protected Pair<String, String> commentStart = null;
+    protected List<Pair<String, String>> commentLines = new ArrayList<Pair<String, String>>();
+    protected Pair<String, String> commentEnd = null;
+
     public CommentBlock(final List<String> data) {
         super(data);
     }
+
     public CommentBlock(final Block data) {
         super(data);
     }
 
     public static CommentBlock createCommentBlock(final List<String> commentText) {
-            return new CommentBlock(commentText);            
+        return new CommentBlock(commentText);
     }
 
     public List<String> contents() {
         List<String> contents = new ArrayList<String>();
-        if(commentStart != null && !commentStart.second().trim().equals(""))
+        if (commentStart != null && !commentStart.second().trim().equals(""))
             contents.add(commentStart.second());
-        for(Pair<String,String> p : commentLines) {
+        for (Pair<String, String> p : commentLines) {
             contents.add(p.second());
         }
-        if(commentEnd!= null && !commentEnd.first().trim().equals(""))
+        if (commentEnd != null && !commentEnd.first().trim().equals(""))
             contents.add(commentEnd.first());
         return contents;
     }
@@ -42,33 +44,34 @@ public class CommentBlock extends Block{
 
     public boolean equals(Object obj) {
         if (obj == this)
-	        return true ;
+            return true;
 
-	    if (!(obj instanceof CommentBlock))
-	        return false ;
+        if (!(obj instanceof CommentBlock))
+            return false;
 
-	    CommentBlock block = (CommentBlock)obj ;
+        CommentBlock block = (CommentBlock) obj;
 
-	    // Equal if contents are equal; we ignore the tags
-        if(!commentStart.equals(block.commentStart)) {
+        // Equal if contents are equal; we ignore the tags
+        if (!commentStart.equals(block.commentStart)) {
             return false;
         }
-        if(!commentEnd.equals(block.commentEnd)) {
+        if (!commentEnd.equals(block.commentEnd)) {
             return false;
         }
-        Iterator<Pair<String,String>> iter1 = commentLines.iterator() ;
-	    Iterator<Pair<String,String>> iter2 = block.commentLines.iterator() ;
-	    while (iter1.hasNext() && iter2.hasNext()) {
-	        Pair<String,String> pair1 = iter1.next() ;
-	        Pair<String,String> pair2 = iter2.next() ;
-	        if (!pair1.equals( pair2 ))
-		        return false ;
-	    }
-	    return iter1.hasNext() == iter2.hasNext() ;
+        Iterator<Pair<String, String>> iter1 = commentLines.iterator();
+        Iterator<Pair<String, String>> iter2 = block.commentLines.iterator();
+        while (iter1.hasNext() && iter2.hasNext()) {
+            Pair<String, String> pair1 = iter1.next();
+            Pair<String, String> pair2 = iter2.next();
+            if (!pair1.equals(pair2))
+                return false;
+        }
+        return iter1.hasNext() == iter2.hasNext();
     }
 
     public static class LineComment extends CommentBlock {
         String prefix;
+
         public LineComment(String prefix, final List<String> data) {
             super(data);
             this.prefix = prefix;
@@ -84,18 +87,18 @@ public class CommentBlock extends Block{
         public static CommentBlock createCommentBlock(String prefix, final List<String> commentText) {
             Block commentTextBlock = new Block(commentText);
             commentTextBlock.addPrefixToAll(prefix);
-            return new LineComment(prefix, commentTextBlock.data);            
+            return new LineComment(prefix, commentTextBlock.data);
         }
 
         public Block replace(Block blockText) {
             commentLines.clear();
-            for(String str: blockText.data) {
+            for (String str : blockText.data) {
                 commentLines.add(new Pair<String, String>(prefix, str));
             }
             // update the block content-view
             blockText.addPrefixToAll(prefix);
             super.replace(blockText);
-            
+
             return this;
         }
 
@@ -112,36 +115,39 @@ public class CommentBlock extends Block{
         String prefix;
         String start;
         String end;
+
         public BlockComment(String start, String end, String prefix, final List<String> data) {
             super(data);
             this.start = start;
             this.end = end;
-            this.prefix =  prefix;
+            this.prefix = prefix;
             parse(data);
         }
+
         public BlockComment(String start, String end, String prefix, final Block dataBlock) {
             super(dataBlock);
             this.start = start;
             this.end = end;
-            this.prefix =  prefix;
+            this.prefix = prefix;
             parse(dataBlock.data);
         }
+
         public static CommentBlock createCommentBlock(String start, String end, String prefix,
                                                       final List<String> commentText) {
             Block commentTextBlock = new Block(commentText);
             commentTextBlock.addPrefixToAll(prefix);
             commentTextBlock.addBeforeFirst(start);
             commentTextBlock.addAfterLast(end);
-            return new BlockComment(start, end, prefix, commentTextBlock.data);            
+            return new BlockComment(start, end, prefix, commentTextBlock.data);
         }
 
         public Block replace(Block blockText) {
-            commentStart = new Pair<String,String>(commentStart.first(), "");
+            commentStart = new Pair<String, String>(commentStart.first(), "");
             commentLines.clear();
-            for(String str: blockText.data) {
+            for (String str : blockText.data) {
                 commentLines.add(new Pair<String, String>(prefix, str));
             }
-            commentEnd = new Pair<String,String>("",commentEnd.second());
+            commentEnd = new Pair<String, String>("", commentEnd.second());
 
             // update the block content-view
             blockText.addPrefixToAll(prefix);
@@ -152,9 +158,9 @@ public class CommentBlock extends Block{
         }
 
         private void parse(List<String> data) {
-            for(int i=0; i<data.size();i++) {
+            for (int i = 0; i < data.size(); i++) {
                 String str = data.get(i);
-                if(i==0) {
+                if (i == 0) {
                     int index = str.indexOf(start);
                     if (index < 0) {
 //                        String startCommmentPrefix = start;
@@ -163,34 +169,43 @@ public class CommentBlock extends Block{
                         throw new RuntimeException("Cooment block does n't caontain start marker " + start);
 
                     } else {
-                        String startCommmentPrefix = str.substring(0, index+start.length());
-                        String startCommentSuffix = str.substring(index + start.length());
+                        String startCommmentPrefix = str.substring(0, index + start.length());
+                        String startCommentSuffix;
+                        String rest = str.substring(index + start.length());
+                        int endindex = rest.indexOf(end);
+                        if (endindex >= 0) {
+                            startCommentSuffix = rest.substring(0, endindex);
+                            String endCommentSuffix = rest.substring(endindex);
+                            commentEnd = new Pair<String, String>("", endCommentSuffix);
+                        } else {
+                            startCommentSuffix = rest;
+                        }
                         commentStart = new Pair<String, String>(startCommmentPrefix, startCommentSuffix);
                     }
-                } else if(i== (data.size()-1)) {
+                } else if (i == (data.size() - 1)) {
                     int index = str.indexOf(end);
-                    if(index < 0) {
+                    if (index < 0) {
 //                        String endCommmentPrefix = str;
 //                        String endCommentSuffix = end;
 //                        commentEnd = new Pair<String,String>(endCommmentPrefix,endCommentSuffix);
                         throw new RuntimeException("Cooment block does n't contain end marker " + end);
                     } else {
-                        String endCommmentPrefix = str.substring(0,index);
+                        String endCommmentPrefix = str.substring(0, index);
                         String endCommentSuffix = str.substring(index);
-                        commentEnd = new Pair<String,String>(endCommmentPrefix,endCommentSuffix);
+                        commentEnd = new Pair<String, String>(endCommmentPrefix, endCommentSuffix);
                     }
                 } else {
                     int index = str.indexOf(prefix);
-                    if(index < 0) {
+                    if (index < 0) {
                         String commmentPrefix = "";
                         String commentSuffix = str;
-                        commentLines.add(new Pair<String,String>(commmentPrefix,commentSuffix));
+                        commentLines.add(new Pair<String, String>(commmentPrefix, commentSuffix));
 
                     } else {
                         String commmentPrefix = str.substring(0, index + prefix.length());
                         String commentSuffix = str.substring(index + prefix.length());
-                        commentLines.add(new Pair<String,String>(commmentPrefix,commentSuffix));
-                    }    
+                        commentLines.add(new Pair<String, String>(commmentPrefix, commentSuffix));
+                    }
                 }
             }
         }
