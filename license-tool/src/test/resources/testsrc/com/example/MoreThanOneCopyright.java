142307906750d948e9b4fe1af42716e35bcfb512
Rama Pulavarthi <ramapulavarthi@java.net>
2008-01-12 07:56:53 +0000

    add one more testcase
    svn path=/trunk/; revision=55

---
+++ license-tool/src/test/resources/testsrc/com/example/MoreThanOneCopyright.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.example;
+/*
+ * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * This is a test class with a different copyright header.
+ */
+public class MoreThanOneCopyright {
+    // foo: dummy int
+    public int foo;
+
+    /**
+     * returns foo.
+     */
+    public int getFoo() {
+
+        /*
+        just retun it
+        without modifying it.
+        */
+
+        return foo;
+    }
+    /*TODO*/
+    public String bar;/*bar is a String*/
+
+}
